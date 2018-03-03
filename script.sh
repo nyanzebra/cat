4bc2cc4db0d5380b6d978e697875680b5386ca04
Robert T Baldwin <robbaldwin95@gmail.com>
2018-03-03 12:30:07 -0800

    f

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ which $CMAKE
 mkdir ./tst/bin
 #$CMAKE -B./tst/bin -H./tst -DCMAKE_BUILD_TYPE=Debug
 cd tst
-$CMAKE . # -DCMAKE_BUILD_TYPE=Debug
+$CMAKE .. -DCMAKE_BUILD_TYPE=Debug
 cd bin
 make clean
 make cat
