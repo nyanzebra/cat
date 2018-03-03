076e60a1e4c52c112d716ce104e12dadc5bfa5fd
Robert T Baldwin <robbaldwin95@gmail.com>
2018-03-03 11:29:32 -0800

    f

--- linux_install.sh
+++ linux_install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 #CMAKE
-echo $DEPS_DIR
 CMAKE_URL="https://cmake.org/files/v3.9/cmake-3.9.1-Linux-x86_64.tar.gz"
 mkdir cmake && wget --no-check-certificate -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-export PATH=${DEPS_DIR}/cmake/bin:${PATH}
+export PATH=/tmp/cmake/bin:${PATH}
 #CODECOV
 apt-get update && apt-get install -y python-pip
 pip install codecov
 #LLVM
-LLVM_INSTALL=${DEPS_DIR}/llvm/install
+LLVM_INSTALL=/tmp/llvm/install
 LLVM_VERSION="3.9.1";
 LLVM_URL="http://llvm.org/releases/${LLVM_VERSION}/llvm-${LLVM_VERSION}.src.tar.xz"
 LIBCXX_URL="http://llvm.org/releases/${LLVM_VERSION}/libcxx-${LLVM_VERSION}.src.tar.xz"
